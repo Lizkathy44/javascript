a0e12e6cb98ef6d9353a82a9da9cb1ad91abf687
Lizkathy44 <lizkathy44@gmail.com>
2022-10-19 19:00:49 +0300

    js fundamentals

---
+++ conditionals.js
@@ -0,0 +1,33 @@
+// days in a week
+let days = 7
+//  Declare password variable and give it a value
+// use if statements to :
+// check if password has space
+// to ensure that your password has a length of more than six characters
+// console.log something if something is true
+
+let password = 'hello kathy';
+if (password.length >=6){
+    if(password.indexOf(' ') ===-1){
+        console.log('VALID PASSWORD');
+    }
+        else{
+           console.log("INVALID PASSWORD");
+        }
+    };
+
+    let newPassword = 'hello kathy';
+if (password.length >=6 && password.indexOf(' ')=== -1);
+    
+    
+        else{
+           console.log("INVALID PASSWORD");
+        }
+    // logical operators
+    let age = 30;
+    if (age<6||age>=65){
+        console.log('you get in for free');
+    }
+    else{
+        console.log(`you need pay ksh.${50} to eat`);
+    }
---
+++ strings.js
@@ -0,0 +1,43 @@
+let animal= "dog";
+console.log(animal.length);
+
+// access individual characters in a string
+console.log(animal[3]);
+
+let msg="i am king";
+console.log(msg);
+let yellMsg= msg.toLocaleUpperCase();
+console.log(yellMsg);
+ let people="student obey teacher";
+ console.log(people);
+ let spPeople=people.toUpperCase();
+ console.log(spPeople);
+ let greeting="leave me alone plz";
+ console.log(greeting);
+ let newGreeting= 'greetings.trim';
+ console.log(newGreeting);
+ let queen="kathy";
+ console.log(queen);
+ let newName=queen.toUpperCase();
+ console.log(newName);
+ let str= 'lokichoggio';
+ console.log(str);
+
+ let partOfStr = str.slice(2, 5);
+ console.log(partOfStr);
+
+//  let partOfStr = str.slice(2, 5);
+//  console.log(partOfStr);
+
+let annoyingLaugh = 'teehee so funny! teehee';
+console.log(annoyingLaugh);
+
+annoyingLaugh.replace('teehee', 'haha')
+let replacer = annoyingLaugh.replaceAll ('teehee', 'haha');
+console.log(replacer);
+
+let myName= 'kathy\nyotto';
+console.log (myName);
+
+
+
