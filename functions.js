21784f86b32ad53390735f94a3388a5d7b40818a
Lizkathy44 <lizkathy44@gmail.com>
2022-11-02 15:03:32 +0300

    second commit

---
+++ functions.js
@@ -0,0 +1,78 @@
+// functiojn is two step function
+// define
+//syntax 
+// function funcName(){
+//     //do something
+// }
+function grumpus(){
+    console.log(' uh..you again...');
+    console.log( 'for the last time..');
+    console.log|( 'leave me alone!!!');
+}
+
+// running/calling/executing
+grumpus(); //you must call a function
+
+// No input/argument
+function greet(){
+    console.log('hi')
+};
+greet();
+// argument/with input
+function greet(name){
+    // name is the argument
+    console.log(`hi, ${name}`);
+}
+greet( 'kathy');   //kathy is the argument
+
+// multiply two numbers
+     //define function     //parameters
+function multiply(x,y){
+     console.log(x*y);
+};
+  function multiply(num1,num2){
+    console.log(num1*num2)
+  };
+    // multiply(5,15);
+    // multiply(5,10);
+    // multiply(20,10);
+// calling outside the curly brackets
+multiply(2,3);  //(2,3)are arguments
+
+// finding the average of numbers using function
+// function avg(arr){
+    //let total = 0
+//}
+
+// divison
+function division (x,y){
+    console.log(x/y);
+};
+division(20,2);
+
+//addition
+function add (x,y){
+    console.log(x+y)
+}
+add(20,2);
+
+//square
+function square(x){
+    console.log(x*x)
+};
+square(20);
+
+function greet(world){
+    console.log(`hello ${world}`.toUpperCase());
+};
+
+let arr1 = [15,20,25,30,50];
+console.log(arr1);
+let arr2 = [10,22,28,34,46];
+console.log(arr2);
+function avg(arr){
+        
+};
+
+
+   
